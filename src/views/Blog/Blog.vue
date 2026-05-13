6ce94a10f8469e6099b473055646aab16ecd43ca
Hgnim <hgn1m@outlook.com>
2026-05-13 18:28:28 +0800

    临时提交

--- src/views/Blog/Blog.vue
+++ src/views/Blog/Blog.vue
@@ -1,7 +1,22 @@
 <script setup lang="ts">
 import {useTitle} from "@vueuse/core";
+import {type BlogInfo, blogListGeter} from "@/views/Blog/ts/blog.ts";
+import {ref, type Ref} from "vue";
 
 useTitle('Hagnimik的博客');
+
+const {init:blgInit,getBlogList}=blogListGeter();
+blgInit().then(()=>{
+  getBlogList().then((val)=>{
+    if (val!=null){
+      val.forEach((v)=>{
+        blogList.value.push(v);//使用push减小性能开销
+      });
+    }
+  });
+});
+
+const blogList:Ref<BlogInfo[]>=ref([]);
 </script>
 
 <template>
@@ -14,13 +29,14 @@ useTitle('Hagnimik的博客');
     <div class="row mt-2">
       <div class="col-10 mx-auto">
         <div class="list-group">
-          <a href="#" class="list-group-item list-group-item-action">
+          <a v-for="(bi,index) in blogList" :key="index"
+             href="#" class="list-group-item list-group-item-action">
             <div class="d-flex w-100 justify-content-between">
-              <h5 class="mb-1">title</h5>
+              <h5 class="mb-1">{{bi.title}}</h5>
             </div>
-            <p class="mb-1">summary</p>
+            <p class="mb-1">{{bi.summary}}</p>
             <div class="d-flex w-100 justify-content-end">
-              <small class="text-body-secondary">time</small>
+              <small class="text-body-secondary">{{bi.time}}</small>
             </div>
           </a>
         </div>
---
+++ src/views/Blog/ts/blog.ts
@@ -0,0 +1,74 @@
+import blogDataBaseUrl from "@/ts/env/blogDataBaseUrl.ts";
+
+//单个博客信息
+export type BlogInfo={
+    //博客id
+    id:number,
+    //博客标题
+    title:string,
+    //博客概要
+    summary:string,
+    //博客时间
+    time:string,
+};
+
+//所有博客的总信息
+export type BlogTotalInfo={
+    //最小博客id索引
+    minIndex:number,
+    //最大博客id索引
+    maxIndex:number,
+}
+
+export function blogListGeter(){
+    //是否已经初始化
+    let isInit=false;
+    //是否正在进行初始化
+    let isInitLoading=false;
+    //是否正在加载博客列表
+    let isBlogListLoading=false;
+
+    //博客总信息
+    let blogTotalInfo:BlogTotalInfo|undefined=undefined;
+    //当前博客索引值
+    let blogIndex:number = -1;
+
+    //初始化
+    async function init(){
+        isInitLoading=true;
+        const res=await fetch(`${blogDataBaseUrl}/blogs/info.json`);
+        if (res.ok){
+            blogTotalInfo=await res.json();
+            blogIndex=blogTotalInfo!.maxIndex;
+            isInit=true;
+        }
+        isInitLoading=false;
+    }
+
+    async function getBlogList(num:number=10):Promise<BlogInfo[]|null>{
+        if (isInit && !isBlogListLoading) {
+            isBlogListLoading=true;
+            const bi:BlogInfo[]=[];
+            for (let i = 0; i < num; i++) {
+                if (blogIndex<blogTotalInfo!.maxIndex){
+                    break;
+                }
+
+                const res=await fetch(`${blogDataBaseUrl}/blogs/${blogIndex}/info.json`);
+                if (res.ok){
+                    bi.push(await res.json());
+                }
+                blogIndex--;
+            }
+            isBlogListLoading=false;
+            return bi;
+        }
+        else
+            return null;
+    }
+
+    return{
+        isInit,isInitLoading,isBlogListLoading,blogIndex,blogTotalInfo,
+        init,getBlogList,
+    };
+}

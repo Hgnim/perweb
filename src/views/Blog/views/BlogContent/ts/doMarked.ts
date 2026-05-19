1bc0c0481b9c63838719aefb318d513f0a4c3a20
Hgnim <hgn1m@outlook.com>
2026-05-20 00:26:47 +0800

    修复了博客内容页的markdown内容预渲染失效的问题

--- src/views/Blog/views/BlogContent/ts/doMarked.ts
+++ src/views/Blog/views/BlogContent/ts/doMarked.ts
@@ -1,7 +1,7 @@
 import {marked} from "marked";
 import blogDataBaseUrl from "@/ts/env/blogDataBaseUrl.ts";
 
-export default async function (sourceStr:string){
+export default function (sourceStr:string):string|Promise<string>{
     return marked(
         sourceStr
             .replace('%%%viteBlogDataBaseUrl%%%',blogDataBaseUrl!)//将markdown中的变量字符串替换成值

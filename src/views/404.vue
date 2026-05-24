b60d66386ca5dcb00db494d991d18ab8752feefc
Hgnim <hgn1m@outlook.com>
2026-05-24 23:50:25 +0800

    新增404页面

---
+++ src/views/404.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import {useTitle} from "@vueuse/core";
+
+useTitle('未找到页面');
+</script>
+
+<template>
+<div id="code404-main" class="d-flex align-items-center justify-content-center">
+  <div>
+    <strong>未找到指定页面</strong>
+  </div>
+</div>
+</template>
+
+<style scoped lang="scss">
+#code404-main{
+  height: 100vh;
+}
+</style>
